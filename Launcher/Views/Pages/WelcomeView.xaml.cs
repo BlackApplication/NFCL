2abe846f508bef6a14e0b67105ad589e617d8c44
Dark Herald <artemshimon2000@gmail.com>
2024-06-20 00:03:27 +0300

    NFCT-27 Added Welcome page

--- Launcher/Views/Pages/WelcomeView.xaml.cs
+++ Launcher/Views/Pages/WelcomeView.xaml.cs
@@ -1,12 +1,14 @@
 ﻿using MvvmCross.Platforms.Wpf.Views;
+using System.Windows;
 
 namespace Launcher.Views.Pages;
 
-
-public partial class WelcomeView : MvxWpfView
-{
-    public WelcomeView()
-    {
+public partial class WelcomeView : MvxWpfView {
+    public WelcomeView() {
         InitializeComponent();
+    }
+
+    private void ShutDownApp(object sender, RoutedEventArgs e) {
+        Application.Current.Shutdown();
     }
 }
--- Launcher/Views/Windows/MainWindow.xaml.cs
+++ Launcher/Views/Windows/MainWindow.xaml.cs
@@ -1,9 +1,16 @@
 ﻿using MvvmCross.Platforms.Wpf.Views;
+using System.Windows.Input;
 
 namespace Launcher.Views.Windows;
 
 public partial class MainWindow : MvxWindow {
     public MainWindow() {
         InitializeComponent();
+    }
+
+    private void WindowMove(object sender, MouseButtonEventArgs e) {
+        if (e.ChangedButton == MouseButton.Left) {
+            this.DragMove();
+        }
     }
 }
